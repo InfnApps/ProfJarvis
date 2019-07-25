cc498f0dc728be192b3c8e7604e41b941fcf7a81
Hallison Oliveira da Paz <hallison.paz@prof.infnet.edu.br>
2019-07-25 20:43:54 -0300

    changed background color of messages according to sender

--- app/src/main/java/br/edu/infnet/professorjarvis/chat/MessageAdapter.kt
+++ app/src/main/java/br/edu/infnet/professorjarvis/chat/MessageAdapter.kt
@@ -1,19 +1,22 @@
 package br.edu.infnet.professorjarvis.chat
 
+import android.graphics.Color
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import br.edu.infnet.professorjarvis.R
 import br.edu.infnet.professorjarvis.chat.model.ChatMessage
+import br.edu.infnet.professorjarvis.commom.jarvisMessageBackground
+import br.edu.infnet.professorjarvis.commom.userMessageBackground
 import kotlinx.android.synthetic.main.message_card.view.*
 
 class MessageAdapter(val items:MutableList<ChatMessage> = mutableListOf())
     : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-
 
     override fun getItemCount() = items.size
 
@@ -27,13 +30,25 @@ class MessageAdapter(val items:MutableList<ChatMessage> = mutableListOf())
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val message = items[position]
         if (holder is MessageViewHolder){
+            //configura texto da mensagem
             holder.bodyTextView.text = message.text
+            //configura hora de envio
             holder.momentTextView.text = message.moment
-            if (message.fromUser){
-                //holder.card.layoutParams.
+
+            // posição da mensagem na tela (esquerda ou direita)
+            holder.card.layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT).apply {
+                gravity = if (message.fromUser) Gravity.END else Gravity.START
+            }
+            // cor de fundo da mensagem
+            holder.card.setCardBackgroundColor(
+                Color.parseColor(
+                    if (message.fromUser) userMessageBackground
+                    else jarvisMessageBackground))
             }
         }
-    }
+
 
     class MessageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val bodyTextView: TextView = itemView.findViewById(R.id.message_textview)
---
+++ app/src/main/java/br/edu/infnet/professorjarvis/commom/Constants.kt
@@ -0,0 +1,4 @@
+package br.edu.infnet.professorjarvis.commom
+
+val userMessageBackground = "#FFE57F"
+val jarvisMessageBackground = "#A7FFEB"
